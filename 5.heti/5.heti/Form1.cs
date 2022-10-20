38ed0d1298acbf09565266e62a98768c894cf4e7
ber.sari22@gmail.com <ber.sari22@gmail.com>
2022-10-20 14:59:10 +0200

    webszolgaltatas

--- 5.heti/5.heti/Form1.cs
+++ 5.heti/5.heti/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5.heti.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,34 @@ namespace _5.heti
         public Form1()
         {
             InitializeComponent();
+
+            harmadik();
+
+
+
         }
+
+        private void harmadik()
+        {
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+            };
+
+            var reponse = mnbService.GetExchangeRates(request);
+
+            var result = reponse.GetExchangeRatesResult();
+
+
+
+        }
+
+        
     }
 }
