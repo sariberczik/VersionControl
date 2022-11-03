13b79a7e71b85ec0975077f77724460e32718e0a
ber.sari22@gmail.com <ber.sari22@gmail.com>
2022-11-03 10:19:32 +0100

    beallitasok

---
+++ 6.heti/6.heti/Entities/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6.heti.Entities
+{
+    class Ball: Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+
+        }
+    }
+}
