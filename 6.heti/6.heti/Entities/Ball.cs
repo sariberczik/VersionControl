d88be3329e6b60e68efa0d754531587fbfa4a939
ber.sari22@gmail.com <ber.sari22@gmail.com>
2022-11-03 10:24:09 +0100

    uj fv

--- 6.heti/6.heti/Entities/Ball.cs
+++ 6.heti/6.heti/Entities/Ball.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,19 @@ namespace _6.heti.Entities
             Width = 50;
             Height = 50;
 
+            Paint += Ball_Paint; //painthez esemenykezelo: paint+=tabtab (throw...törlése)
+
         }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        void DrawImage(Graphics graphics) //új függvényt DrawImage néven és Graphics típusú bemeneti paraméterrel, villanykorte
+        {
+
+        }
+        
     }
 }
