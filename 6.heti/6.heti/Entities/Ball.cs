acd8668def0587c47f571264147d47de43e6500e
ber.sari22@gmail.com <ber.sari22@gmail.com>
2022-11-03 10:33:52 +0100

    labda letrehozasa

--- 6.heti/6.heti/Entities/Ball.cs
+++ 6.heti/6.heti/Entities/Ball.cs
@@ -26,10 +26,18 @@ namespace _6.heti.Entities
 
         }
 
-        void DrawImage(Graphics graphics) //új függvényt DrawImage néven és Graphics típusú bemeneti paraméterrel, villanykorte
+        protected void DrawImage(Graphics graphics) //új függvényt DrawImage néven és Graphics típusú bemeneti paraméterrel, villanykorte
+        //protected: ha nem a fv-re van szükségem, csak arr h hasznalhassam
         {
             graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height); //kitöltött kék kör
         }
-        
+
+        public void MoveBall() //publikus fv
+        {
+            Left++; //1gyel növelem
+
+        }
+
+
     }
 }
