764245c4cc61d424038eec84e0afb93f41da9ff1
ber.sari22@gmail.com <ber.sari22@gmail.com>
2022-11-08 12:43:00 +0100

    portfoloioitem class letrehozasa

--- 7.heti/7.heti/Form1.Designer.cs
+++ 7.heti/7.heti/Form1.Designer.cs
@@ -36,9 +36,9 @@ namespace _7.heti
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(120, 92);
+            this.dataGridView1.Location = new System.Drawing.Point(49, 57);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(516, 310);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
