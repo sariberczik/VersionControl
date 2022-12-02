6fbc58b502d9a2f7d538903143e6f2e069559759
ber.sari22@gmail.com <ber.sari22@gmail.com>
2022-12-02 18:53:19 +0100

    birthtartalom

--- 9.heti/9.heti/BirthProbability.cs
+++ 9.heti/9.heti/BirthProbability.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace _9.heti
 {
-    class BirthProbability
+    public class BirthProbability
     {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
     }
 }
